fe958f19d33d6535f6a0ad1e388911a86a19a553
accell <me@ericcolon.com>
2015-09-20 22:03:41 -0400

    removed useless files, form changes

--- _src/assets/js/bastide/mailing-list.js
+++ _src/assets/js/bastide/mailing-list.js
@@ -9,53 +9,66 @@ Bastide.mailingList = {
     }
   },
   subscribe: function() {
-    var email = document.querySelector("#mailing-list [name=email]").value;
+    var form  = document.querySelector('#mailing-list');
+    var email = document.querySelector('#mailing-list [name=email]').value;
 
     if (!email) {
-      if(document.querySelector('#mailing-list .alert-box')) {
-        document.querySelector('#mailing-list .alert-box').remove();
+      if (form.querySelector('.alert-box')) {
+        form.querySelector('.alert-box').remove();
       }
 
-      document.querySelector('#mailing-list').className = "error";
-      document.querySelector('#mailing-list')
-        .insertAdjacentHTML(
-          'afterbegin',
-          '<div class="alert-box alert text-left">Please enter a valid email address</div>'
-        );
+      form.className = 'error';
+      form.insertAdjacentHTML(
+        'afterbegin',
+        '<div class="alert-box alert text-left">Please enter a valid email address</div>'
+      );
 
       return;
     }
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open("POST", "/api/mailinglist/subscribe");
-    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.open('POST', '/api/mailinglist/subscribe');
+    xhr.setRequestHeader('Content-type', 'application/json');
     xhr.send(JSON.stringify({ email: email }));
 
     xhr.onload = function() {
-      if (xhr.status !== 200 || !xhr.responseText || xhr.responseText.indexOf("youtube.com") === -1) {
-        if(document.querySelector('#mailing-list .alert-box')) {
-          document.querySelector('#mailing-list .alert-box').remove();
+      if (xhr.status !== 200 || !xhr.responseText || xhr.responseText.indexOf('youtube.com') === -1) {
+        if (form.querySelector('.alert-box')) {
+          form.querySelector('.alert-box').remove();
         }
 
-        document.querySelector('#mailing-list').className = "error";
-        document.querySelector('#mailing-list')
-          .insertAdjacentHTML(
-            'afterbegin',
-            '<div class="alert-box alert text-left">Oh no! The email signup API seems to be down. Shoot us an email or let us know on Facebook\nHow embarassing.</div>'
-          );
+        form.className = 'error';
+        form.insertAdjacentHTML(
+          'afterbegin',
+          '<div class="alert-box alert text-left">The email signup API seems to be down. Shoot us an email or let us know on Facebook.</div>'
+        );
 
         return;
       }
 
-      var btn = document.getElementById("signupButton");
+      var btn = form.querySelector('[type=submit]');
 
-      btn.className += " done";
+      btn.className += 'done';
       btn.disabled   = true;
-      btn.innerHTML  = "Done!";
+      btn.innerHTML  = 'Done!';
 
       setTimeout(function() {
-        document.querySelector("#mailing-list").className = "success disabled";
+        form.insertAdjacentHTML(
+          'afterbegin',
+          '<div class="alert-box success text-left">Thanks for signing up!</div>'
+        );
+
+        labels = form.querySelectorAll('label');
+        fields = form.querySelectorAll('input, textarea, select');
+
+        [].forEach.call(labels, function(label) {
+          label.classList.add('disabled');
+        });
+
+        [].forEach.call(fields, function(field) {
+          field.disabled = true;
+        });
       }, 1000);
     };
   }
